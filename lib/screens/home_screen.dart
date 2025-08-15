4e35dd13c0d665668a330aa1c3516d7556908df2
Hari Ram <hariramreddy6204@gmail.com>
2025-08-15 11:41:44 +0530

    test update feature in app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -181,7 +181,7 @@ class _HomePageState extends State<HomePage> {
         foregroundColor: Colors.black,
         centerTitle: true,
         title: const Text(
-          'Voice Intell',
+          'Voice Intelligence',
 
           style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
         ),
